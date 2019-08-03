e207f46e5534c6842baf4c643935f7cdd3b6d9a6
wesleyramos <wesleyramos523@gmail.com>
2019-08-02 23:43:07 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Smart Learn
+
+Smart Learn is a useful API to customize the learning by questions.
---
+++ src/test/kotlin/com/wesley/smartlearn/SmartLearnApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.wesley.smartlearn
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SmartLearnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
